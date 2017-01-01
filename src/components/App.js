5c52103e1b2dbce0ef0ebe532342b59a2a91393b
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-01-01 23:47:52 +0900

    Add top page

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router';
 import {version} from '../../package.json';
+import Home from './Home';
 
 const App = ({children}) => (
     <div>
         <header>
-            <h1>TANAKA WORLD</h1>
+            <h1>
+                <Link to="/">TANAKA WORLD</Link>
+            </h1>
             <Link to="/about">About</Link>
             <Link to="/gallery">Gallery</Link>
         </header>
         <section>
-            {children || 'Welcome to React Starterify'}
+            {children || <Home/>}
         </section>
     </div>
 );
