f3587e175f714b29eb5acde2e173ce8638e1df6d
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-04-09 14:21:11 +0900

    Update about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
 const About = () => (
-    <div>
-        <h2>About</h2>
-        <p>React Starterify aims to give you a good starting point for your projects.</p>
+    <div id="about">
+
         <p>
-            If you're looking for a minimal ES6 (ES2015) React JS starter
-            with nice shallow rendering test examples, this is probably for you.
+            <a href="http://tanaka.world">tanakaworld</a> is a Software Enginner in Japan.
         </p>
+
+        <h2>Accounts</h2>
+        <ul>
+            <li><a href="https://twitter.com/_tanakaworld">Twitter</a></li>
+            <li><a href="https://github.com/tanakaworld">GitHub</a></li>
+            <li><a href="http://qiita.com/tanakaworld">Qiita (Short Tips)</a></li>
+            <li><a href="https://www.facebook.com/tanaka.world">Facebook</a></li>
+        </ul>
+
+
+        <h2>History</h2>
+        <ul>
+            <li>2016 - Present  Software Engineer - <a href="http://www.curiosity-inc.jp/">curiosity, inc.</a></li>
+            <li>2016 - Present  Engineer - <a href="https://returnees.jp/">Returnees, inc.</a></li>
+            <li>2015 - 2016  Web Developer - <a href="http://fukurou-labo.co.jp/">Fukurou Labo, inc.</a></li>
+            <li>2013 - 2015  Web Developer - <a href="http://www.mti.co.jp/">MTI Ltd.</a></li>
+        </ul>
+
+        {/* TODO */}
+        {/*<h2>Skills</h2>*/}
+        {/*<ul>*/}
+            {/*<li>Ruby</li>*/}
+            {/*<li>JavaScript</li>*/}
+            {/*<li>Node.js</li>*/}
+            {/*<li>React.js</li>*/}
+        {/*</ul>*/}
+
     </div>
 );
 
