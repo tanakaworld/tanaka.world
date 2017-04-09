c69ea2ca7f7af3d2d5f750a6733005b04d7fe18f
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-04-09 14:36:43 +0900

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => (
             <a href="http://tanaka.world">tanakaworld</a> is a Software Enginner in Japan.
         </p>
 
-        <h2>Accounts</h2>
+        {/*<h2>Accounts</h2>*/}
         <ul>
             <li><a href="https://twitter.com/_tanakaworld">Twitter</a></li>
             <li><a href="https://github.com/tanakaworld">GitHub</a></li>
@@ -16,15 +16,14 @@ const About = () => (
         </ul>
 
 
-        <h2>History</h2>
-        <ul>
-            <li>2016 - Present  Software Engineer - <a href="http://www.curiosity-inc.jp/">curiosity, inc.</a></li>
-            <li>2016 - Present  Engineer - <a href="https://returnees.jp/">Returnees, inc.</a></li>
-            <li>2015 - 2016  Web Developer - <a href="http://fukurou-labo.co.jp/">Fukurou Labo, inc.</a></li>
-            <li>2013 - 2015  Web Developer - <a href="http://www.mti.co.jp/">MTI Ltd.</a></li>
-        </ul>
+        {/*<h2>History</h2>*/}
+        {/*<ul>*/}
+            {/*<li>2016 - Present  Software Engineer - <a href="http://www.curiosity-inc.jp/">curiosity, inc.</a></li>*/}
+            {/*<li>2016 - Present  Engineer - <a href="https://returnees.jp/">Returnees, inc.</a></li>*/}
+            {/*<li>2015 - 2016  Web Developer - <a href="http://fukurou-labo.co.jp/">Fukurou Labo, inc.</a></li>*/}
+            {/*<li>2013 - 2015  Web Developer - <a href="http://www.mti.co.jp/">MTI Ltd.</a></li>*/}
+        {/*</ul>*/}
 
-        {/* TODO */}
         {/*<h2>Skills</h2>*/}
         {/*<ul>*/}
             {/*<li>Ruby</li>*/}
