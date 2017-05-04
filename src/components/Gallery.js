565cacbbad32ecbf82f8828b1f56145742a7114d
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-05-04 23:23:15 +0900

    Dynamically render images in gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,101 +1,35 @@
 import React from 'react';
 
-const Gallery = () => (
-    <div id="gallery">
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/me-real.png"/>
-            <p className="description">me</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/ForXDevelopersLogo.png"/>
-            <p className="description">Blog Logo</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me.png"/>
+const Gallery = () => {
 
-            <p className="description">2D</p>
+    const contents = [
+        {description: 'me', file: 'images/me/me-real.png'},
+        {description: 'Blog Logo', file: 'images/me/ForXDevelopersLogo.png'},
+        {description: '2D', file: 'images/me/8bit-me.png'},
+        {description: '3D', file: 'images/me/8bit-me-3d.png'},
+        {description: 'santa/snow', file: 'images/me/8bit-me-santa.png'},
+        {description: 'sheep suit', file: 'images/me/8bit-me-sheep-suit.png'},
+        {description: 'sheep', file: 'images/me/8bit-me-sheep.png'},
+        {description: 'block me clear', file: 'images/me/8bit_me_clear.jpg'},
+        {description: 'block me no cap', file: 'images/me/8bit_me_nocap.png'},
+        {description: 'MacBook', file: 'images/me/mac_sq.png'},
+        {description: 'me', file: 'images/me/me-pixel-all.png'},
+        {description: 'my face', file: 'images/me/me-pixel-face.png'},
+        {description: 'Santa 2015', file: 'images/me/8bit-me-santa-2015.png'},
+        {description: 'Saru Costume 2016', file: 'images/me/8bit-me-mon-2016-costume.png'},
+        {description: 'Saru me 2016', file: 'images/me/8bit-me-mon-2016.png'},
+        {description: 'GIF.me', file: 'images/me/me-deformed-16fr.gif'},
+        {description: 'Tori me 2017', file: 'images/me/me-2017-tori-v1.0.0.500.png'},
+        {description: 'Fly up 2017', file: 'images/me/me-2017-tori-fly-up.500.gif'},
+        {description: 'Sequential Human', file: 'images/me/CharacterAnimationGIF-1.gif'},
+    ].map((c, i) => {
+        return <div className="thumbnail" key={i}>
+            <img className="gallery-icon" src={c.file}/>
+            <p className="description">{c.description}</p>
         </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me-3d.png"/>
+    });
 
-            <p className="description">3D</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me-santa.png"/>
-
-            <p className="description">santa/snow</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me-sheep-suit.png"/>
-
-            <p className="description">sheep suit</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me-sheep.png"/>
-
-            <p className="description">sheep</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit_me_clear.jpg"/>
-
-            <p className="description">block me clear</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit_me_nocap.png"/>
-
-            <p className="description">block me no cap</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/mac_sq.png"/>
-
-            <p className="description">MacBook</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/me-pixel-all.png"/>
-
-            <p className="description">me</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/me-pixel-face.png"/>
-
-            <p className="description">my face</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me-santa-2015.png"/>
-
-            <p className="description">Santa 2015</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me-mon-2016-costume.png"/>
-
-            <p className="description">Saru Costume 2016</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/8bit-me-mon-2016.png"/>
-
-            <p className="description">Saru me 2016</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/me-deformed-16fr.gif"/>
-
-            <p className="description">GIF.me</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/me-2017-tori-v1.0.0.500.png"/>
-
-            <p className="description">Tori me 2017</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/me-2017-tori-fly-up.500.gif"/>
-
-            <p className="description">Fly up 2017</p>
-        </div>
-        <div className="thumbnail">
-            <img className="gallery-icon" src="images/me/CharacterAnimationGIF-1.gif"/>
-
-            <p className="description">Sequential Human</p>
-        </div>
-    </div>
-);
+    return <div id="gallery">{contents}</div>;
+};
 
 export default Gallery;
