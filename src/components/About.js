991991745f5208c380b84d4013b3fed98d1c4f38
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-09-19 21:20:50 +0900

    Update about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,38 +1,91 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const About = () => (
-    <div id="about">
+const unownText = (text) => {
+    return text.split('').map(t => 'unown' + t).join('');
+};
 
-        <p>
-            <a href="http://tanaka.world">tanakaworld</a> is a Software Enginner in Japan.
-        </p>
+export default class About extends Component {
+    constructor(props) {
+        super(props);
 
-        {/*<h2>Accounts</h2>*/}
-        <ul>
-            <li><a href="https://twitter.com/_tanakaworld">Twitter</a></li>
-            <li><a href="https://github.com/tanakaworld">GitHub</a></li>
-            <li><a href="http://qiita.com/tanakaworld">Qiita (Short Tips)</a></li>
-            <li><a href="https://www.facebook.com/tanaka.world">Facebook</a></li>
-        </ul>
+        this.state = {
+            inputText: ''
+        };
+        this._updateInputText = this._updateInputText.bind(this);
+    }
 
+    _updateInputText(e) {
+        this.setState({
+            inputText: e.currentTarget.value
+        })
+    }
 
-        {/*<h2>History</h2>*/}
-        {/*<ul>*/}
-            {/*<li>2016 - Present  Software Engineer - <a href="http://www.curiosity-inc.jp/">curiosity, inc.</a></li>*/}
-            {/*<li>2016 - Present  Engineer - <a href="https://returnees.jp/">Returnees, inc.</a></li>*/}
-            {/*<li>2015 - 2016  Web Developer - <a href="http://fukurou-labo.co.jp/">Fukurou Labo, inc.</a></li>*/}
-            {/*<li>2013 - 2015  Web Developer - <a href="http://www.mti.co.jp/">MTI Ltd.</a></li>*/}
-        {/*</ul>*/}
+    render() {
+        return (
+            <div id="about">
+                <div className="profile-wrap">
+                    <div className="profile-row">
+                        <div>
+                            <img
+                                className="profile-icon"
+                                alt='profile-icon'
+                                src='https://firebasestorage.googleapis.com/v0/b/tanaka-world.appspot.com/o/lp%2Fme%2F8bit_me_clear.jpg?alt=media&token=165e056b-d97b-4a15-81fd-1c8dbb1bdc51'/>
+                        </div>
+                        <div className="profile-property">
+                            <div><a href="http://tanaka.world">tanakaworld</a></div>
+                            <div>Software Engineer</div>
+                            <div>
+                                たかさ 1.75m
+                            </div>
+                            <div>
+                                おもさ 62.0kg
+                            </div>
+                        </div>
+                    </div>
 
-        {/*<h2>Skills</h2>*/}
-        {/*<ul>*/}
-            {/*<li>Ruby</li>*/}
-            {/*<li>JavaScript</li>*/}
-            {/*<li>Node.js</li>*/}
-            {/*<li>React.js</li>*/}
-        {/*</ul>*/}
+                    <div className="profile-row">
+                        あおやまがくいんだいがくりこうがくぶじょうほうテクノロジーがっかそつ。<br/>
+                        エムティーアイ、フクロウラボ、こじんじぎょうぬしをへて2016ねん8がつ curiosity, inc. ににゅうしゃ。エンジニアとして、
+                        Webのフロントエンドからインフラまでおおくのプロジェクトにたずわる。<br/>
+                        JavaScript・C#・Rubyを用いたアプリケーションかいはつをとくいとしている。<br/>
+                        ピクセルアートとルービックキューブをこよなく愛する。
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="https://twitter.com/_tanakaworld">Twitter</a></li>
+                            <li><a href="https://github.com/tanakaworld">GitHub</a></li>
+                            <li><a href="http://qiita.com/tanakaworld">Qiita (Short Tips)</a></li>
+                            <li><a href="https://www.facebook.com/tanaka.world">Facebook</a></li>
+                        </ul>
+                    </div>
+                    <div className="profile-row">
+                        <p>
+                            <a href="http://tanaka.world">tanakaworld</a> is a Software Engineer in Japan.
+                        </p>
+                    </div>
+                    <div className="profile-row">
+                        <p>
+                            <a href="http://tanaka.world">{unownText('tanakaworld')}</a> {unownText('is')} {unownText('a')} {unownText('software')} {unownText('engineer')} {unownText('in')} {unownText('japan')}.
+                        </p>
+                    </div>
+                    <div className="profile-row">
+                        <p>
+                            たなかわーるどは日本のソフトウェアエンジニアです。
+                        </p>
+                    </div>
+                </div>
+                {/*<hr/>*/
+                }
 
-    </div>
-);
-
-export default About;
+                {/*<input type='text' placeholder='enter some text ...' onChange={this._updateInputText}/>*/
+                }
+                {/*<pre>*/
+                }
+                {/*{unownText(this.state.inputText)}*/
+                }
+                {/*</pre>*/
+                }
+            </div>
+        )
+    }
+}
