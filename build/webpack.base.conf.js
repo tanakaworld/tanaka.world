493a4bcb2a0245b9cbb29d6c14fc1bea286f55fe
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-03-17 23:16:46 +0900

    Fix issue that fonts won't be loaded

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -79,10 +79,6 @@ module.exports = {
         test: /\.(sass|css)$/,
         loaders: ['style-loader', 'css-loader', 'sass-loader'],
       },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000',
-      },
     ],
   },
   node: {
