2c7d0ab7af3cb17d749356ab84b12066bdf723bd
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-03-18 01:11:50 +0900

    Hide gallery

--- src/components/common/HeaderView.vue
+++ src/components/common/HeaderView.vue
@@ -10,11 +10,11 @@
         <li>
           <router-link to="/about">About</router-link>
         </li>
-        <li>
-          <router-link to="/gallery">
-            Gallery
-          </router-link>
-        </li>
+        <!--<li>-->
+          <!--<router-link to="/gallery">-->
+            <!--Gallery-->
+          <!--</router-link>-->
+        <!--</li>-->
       </ul>
     </nav>
   </header>
