c8f8163c947127fa92d0afda779a5ae8ef1fa883
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-03-18 01:10:32 +0900

    Fix build errors

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -91,9 +91,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         return (
           module.resource &&
           /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules'),
-          ) === 0
+          module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
         );
       },
     }),
@@ -127,27 +125,22 @@ const webpackConfig = merge(baseWebpackConfig, {
       // List of endpoints you wish to prerender
       [
         '/',
-      ],
-    ),
+        '/about/',
+        '/gallery',
+      ]),
   ],
 });
 
 if (config.build.productionGzip) {
   const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
-  webpackConfig.plugins.push(
-    new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$',
-      ),
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-  );
+  webpackConfig.plugins.push(new CompressionWebpackPlugin({
+    asset: '[path].gz[query]',
+    algorithm: 'gzip',
+    test: new RegExp('\\.(' + config.build.productionGzipExtensions.join('|') + ')$'),
+    threshold: 10240,
+    minRatio: 0.8,
+  }));
 }
 
 if (config.build.bundleAnalyzerReport) {
