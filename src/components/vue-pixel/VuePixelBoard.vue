dea317caf203eed0248cd0850bed080c8e41d8c2
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-05-16 17:02:19 +0900

    Handle touch sequence

--- src/components/vue-pixel/VuePixelBoard.vue
+++ src/components/vue-pixel/VuePixelBoard.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <div class="board" v-for="(s, i) in seed" :key="`board.${i}`">
+    <div class="board" v-for="(s, i) in seed"
+         :key="`board.${i}`"
+         @touchmove="handleTouchMove($event)">
       <div class="row">
         <vue-pixel-xel v-for="(xel, j) in s" :key="`row.${j}`"
+                       :ref="`xel.${i}.${j}`"
+                       :data-xel-id="`xel.${i}.${j}`"
                        :before-color="xel.before"
                        :after-color="xel.after"
                        :static="xel.static === true"
@@ -29,6 +33,19 @@
         type: Boolean,
         required: false,
         default: false,
+      },
+    },
+    methods: {
+      handleTouchMove(e) {
+        if (e && e.target) {
+          const { pageX, pageY } = e;
+          const xelId = document.elementFromPoint(pageX, pageY).dataset.xelId;
+          const xelComponent = this.$refs[xelId];
+
+          if (xelComponent) {
+            xelComponent[0].handleHover();
+          }
+        }
       },
     },
   };
