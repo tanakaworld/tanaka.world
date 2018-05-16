f95f796cdad5bdd49964215699d137565e1ed11a
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-05-16 18:09:00 +0900

    Make pixels fit to the window

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <header-view/>
-    <vue-pixel/>
+    <header-view :ref="'headerView'"/>
+    <vue-pixel class="vue-pixel" :style="vuePixelStyles"/>
   </div>
 </template>
 
@@ -11,13 +11,51 @@
 
   export default {
     name: 'home',
+    data() {
+      return {
+        vuePixelWidth: 0,
+        vuePixelWidthMax: 600,
+        vuePixelHeight: 0,
+      };
+    },
     components: {
       HeaderView,
       VuePixel,
     },
+    methods: {
+      updateViePixelHeight() {
+        if (window) {
+          const windowHeight = window.innerHeight;
+          const windowWidth = window.innerWidth;
+          const headerHeight = this.$refs['headerView'].$el.clientHeight;
+          // calc height
+          this.vuePixelHeight = windowHeight - headerHeight;
+          // calc width
+          let tmpWidth = this.vuePixelHeight * 14 / 16;
+          if (tmpWidth > windowWidth) tmpWidth = windowWidth;
+          this.vuePixelWidth = tmpWidth;
+        }
+      },
+    },
+    mounted() {
+      window.addEventListener('resize', this.updateViePixelHeight);
+      this.updateViePixelHeight();
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.updateViePixelHeight);
+    },
+    computed: {
+      vuePixelStyles() {
+        return {
+          width: `${this.vuePixelWidth}px`,
+          height: `${this.vuePixelHeight}px`,
+        };
+      },
+    },
   };
 </script>
 
-<style scoped>
-
+<style scoped lang="sass">
+  .vue-pixel
+    margin: 0 auto
 </style>
