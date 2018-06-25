655bb0baad2bc5a673a826d2bc5ffe85e356309d
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-06-26 01:04:19 +0900

    Update about page

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -15,6 +15,7 @@
               <router-link to="http://tanaka.world">tanakaworld</router-link>
             </div>
             <div>Software Engineer</div>
+            <div>LV.{{level}}</div>
             <div>
               たかさ 1.75m
             </div>
@@ -24,14 +25,10 @@
           </div>
         </div>
         <div class="profile-row">
-          あおやまがくいんだいがくりこうがくぶじょうほうテクノロジーがっかそつ。<br/>
-          <a href='http://www.mti.co.jp/' target='_blank'>エムティーアイ</a>、
-          <a href='http://fukurou-labo.co.jp/' target='_blank'>フクロウラボ</a>、
-          こじんじぎょうぬしをへて、2016ねん8がつ
-          <a href='https://www.curiosity-inc.jp/' target='_blank'>curiosity, inc.</a> ににゅうしゃ。エンジニアとして、
-          Webのフロントエンドからインフラまでおおくのプロジェクトにたずわる。<br/>
-          JavaScript・C#・Rubyをもちいたアプリケーションかいはつをとくいとしている。<br/>
-          ピクセルアート、ルービックキューブ、ピクトグラム、イヌをこよなくあいする。
+          エンジニアとして、 Webのフロントエンドからインフラまでおおくのプロジェクトにたずわる。
+          JavaScript・C#・Rubyをもちいたアプリケーションかいはつをとくいとしている。
+          ピクセルアート、ルービックキューブ、ピクトグラム、イヌをこよなく愛する。
+          しゅみは、<a href="https://www.youtube.com/watch?v=qpkCstorMxs&t=1207s" target="_blank">パズル</a>・しょうぎ（ウォーズ２だん）。
         </div>
         <div class="profile-row no-border sns-links-wrap">
           <div v-for="(account, index) in snsAccounts" :key="index">
@@ -39,6 +36,13 @@
                target="_blank">
               {{account.name}}
             </a>
+          </div>
+        </div>
+        <div class="profile-row no-border histories-wrap">
+          <div v-for="(history, index) in histories" :key="index" class="history-item">
+            <div class="history-item-range">{{history.range}}</div>
+            <div><a class="history-item-link" :href="history.link" target='_blank'>{{history.linkLabel}}</a></div>
+            <div v-if="history.role" class="history-item-role">- {{history.role}}</div>
           </div>
         </div>
       </div>
@@ -77,6 +81,40 @@
           { name: 'LinkedIn', url: 'https://www.linkedin.com/in/yutaro-tanaka-world-7863b249/' },
           { name: 'RubyGems', url: 'https://rubygems.org/profiles/tanakaworld' },
         ];
+      },
+      histories() {
+        return [
+          { range: '2013.4 ~ 2015.2', link: 'http://www.mti.co.jp/', linkLabel: 'MTI Ltd.', role: 'Web Developer' },
+          {
+            range: '2015.3 ~ 2016.7',
+            link: 'http://fukurou-labo.co.jp/',
+            linkLabel: 'Fukurou Labo',
+            role: 'Web Developer',
+          },
+          {
+            range: '2016.2 ~',
+            link: 'https://tanaka.world',
+            linkLabel: 'TANAKA WORLD (Self Employed Project)',
+            role: null,
+          },
+          {
+            range: '2016.8 ~ 2017.8',
+            link: 'https://returnees.jp/',
+            linkLabel: 'RETURNEES, Inc.',
+            role: 'Lead Engineer',
+          },
+          {
+            range: '2016.8 ~',
+            link: 'https://www.curiosity-inc.jp/',
+            linkLabel: 'curiosity, inc.',
+            role: 'Web Developer',
+          },
+          { range: '2017.8 ~', link: 'http://iki.mn/', linkLabel: 'IKIMONO Inc.', role: 'Engineer' },
+        ];
+      },
+      level() {
+        const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 31556925994);
+        return getAge('1990-11-07');
       },
     },
   };
@@ -175,4 +213,27 @@
     +sns-link('qiita', #72C23A)
     +sns-link('linkedin', #3175B0)
     +sns-link('rubygems', #D96149)
+
+  .histories-wrap
+    .history-item
+      display: flex
+      justify-content: flex-start
+      align-items: center
+      flex-wrap: wrap
+
+      &-link
+        margin-left: 16px
+      &-role
+        margin-left: 8px
+
+  @media screen and (max-width: 560px)
+    .histories-wrap
+      font-size: 16px
+      .history-item
+        &:not(:first-child)
+          margin-top: 8px
+        &-range
+          width: 100%
+        &-link
+          margin-left: 0
 </style>
