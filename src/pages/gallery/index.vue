493601a5801cb358407ab73d231c9de30382831d
tanakaworld <yutaro.tanaka.world@gmail.com>
2019-12-31 00:05:10 +0900

    refactor: move gallery photos as util

--- src/pages/gallery/index.vue
+++ src/pages/gallery/index.vue
@@ -13,6 +13,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import HeaderView from '~/components/common/HeaderView.vue';
+import { photos } from '~/utils/gallery';
 
 const title = 'Gallery - The TANAKA WORLD';
 const description = 'Arts made by me';
@@ -41,148 +42,7 @@ export default Vue.extend({
   },
   data() {
     return {
-      photos: [
-        {
-          description: 'me',
-          file: '/img/me-real.png'
-        },
-        {
-          description: 'Blog Logo',
-          file: '/img/ForXDevelopersLogo.png'
-        },
-        {
-          description: '2D',
-          file: '/img/tanakaworld-2d.png'
-        },
-        {
-          description: 'Dash',
-          file: '/img/tanakaworld-2d-dash.gif'
-        },
-        {
-          description: 'santa',
-          file: '/img/8bit-me-santa.png'
-        },
-        {
-          description: '2015 未',
-          file: '/img/8bit-me-sheep.png'
-        },
-        {
-          description: '2014 Me',
-          file: '/img/8bit_me_clear.jpg'
-        },
-        {
-          description: 'MacBook',
-          file: '/img/mac_sq.png'
-        },
-        {
-          description: 'Megumi Katoh',
-          file: '/img/megumi-katoh.png'
-        },
-        {
-          description: 'Full Body',
-          file: '/img/me-pixel-all.png'
-        },
-        {
-          description: 'Face',
-          file: '/img/me-real-face.png'
-        },
-        {
-          description: 'Face (secret)',
-          file: '/img/me-real-face-secret.png'
-        },
-        {
-          description: '2015 X-mas',
-          file: '/img/8bit-me-santa-2015.png'
-        },
-        {
-          description: '2016 申',
-          file: '/img/8bit-me-mon-2016.png'
-        },
-        {
-          description: '2017 酉',
-          file: '/img/me-2017-tori-v1.0.0.500.png'
-        },
-        {
-          description: 'ツラタン',
-          file: '/img/tsura-tang.png'
-        },
-        {
-          description: 'Fly up 2017',
-          file: '/img/me-tori-2017-gif.gif'
-        },
-        {
-          description: 'Humans Seq',
-          file: '/img/sequence-human.gif'
-        },
-        {
-          description: 'Cap',
-          file: '/img/me-real-face-cap.png'
-        },
-        {
-          description: '2018 Me',
-          file: '/img/tanakaworld-2018.png'
-        },
-        {
-          description: '2018 No Cap',
-          file: '/img/tanakaworld-2018-no-cap.png'
-        },
-        {
-          description: '2018 Naked',
-          file: '/img/tanakaworld-2018-nkd.png'
-        },
-        {
-          description: '2018 Halloween',
-          file: '/img/tanakaworld-2018-helloween.png'
-        },
-        {
-          description: 'Chick',
-          file: '/img/hiyoko.png'
-        },
-        {
-          description: '2018 戌',
-          file: '/img/tanakaworld-2018-inu.png'
-        },
-        {
-          description: '2019 亥',
-          file: '/img/tanakaworld-2019-iee.png'
-        },
-        {
-          description: '@__syumai',
-          file: '/img/syumai.png'
-        },
-        {
-          description: '@tenntenn',
-          file: '/img/tenntenn.png'
-        },
-        {
-          description: '@swaaan',
-          file: '/img/swaaan-sm.png'
-        },
-        {
-          description: '@swaaan',
-          file: '/img/swaaan-dash.png'
-        },
-        {
-          description: '@codehex',
-          file: '/img/codehex.png'
-        },
-        {
-          description: '@8saki',
-          file: '/img/8saki.png'
-        },
-        {
-          description: 'Ms. Dyamond',
-          file: '/img/ms-dia.png'
-        },
-        {
-          description: '@peranikov',
-          file: '/img/peranikov.png'
-        },
-        {
-          description: '@denoland',
-          file: '/img/deno.gif'
-        }
-      ]
+      photos
     };
   }
 });
---
+++ src/types/app/gallery.d.ts
@@ -0,0 +1,4 @@
+export interface Photo {
+  description: string;
+  file: string;
+}
