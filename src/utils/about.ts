3f0db0c2a818421bd134bd9e0efa8c1c2a325632
tanakaworld <yutaro.tanaka.world@gmail.com>
2020-01-18 20:21:47 +0900

    feat: update text

--- src/utils/about.ts
+++ src/utils/about.ts
@@ -48,7 +48,7 @@ export const snsAccounts: Account[] = [
   { name: 'Blog', url: 'https://blog.tanaka.world' },
   { name: 'Twitter', url: 'https://twitter.com/_tanakaworld' },
   { name: 'Github', url: 'https://github.com/tanakaworld' },
-  { name: 'Slides', url: 'https://speakerdeck.com/tanakaworld' },
+  { name: 'Speaker Deck', url: 'https://speakerdeck.com/tanakaworld' },
   { name: 'Qiita', url: 'http://qiita.com/tanakaworld' },
   { name: 'Facebook', url: 'https://www.facebook.com/tanaka.world' },
   {
